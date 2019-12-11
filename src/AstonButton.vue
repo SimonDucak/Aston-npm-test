42fa81abb5c8693a36871fdbec59a54e389da5a7
SimonDucak <46250370+SimonDucak@users.noreply.github.com>
2019-12-11 10:05:41 +0100

    update

--- src/AstonButton.vue
+++ src/AstonButton.vue
@@ -6,16 +6,3 @@
       name: "AstonButton"
     }
 </script>
-
-<style scoped>
-  button {
-    height: 65px;
-    width: 200px;
-    background-color: black;
-    position: fixed;
-    top: 0;
-    left: 0;
-    color: white;
-    font-weight: bold;
-  }
-</style>
