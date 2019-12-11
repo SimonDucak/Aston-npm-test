4071700914a878398fd685357f3170d084eba782
SimonDucak <46250370+SimonDucak@users.noreply.github.com>
2019-12-11 10:56:21 +0100

    update

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -1,7 +1,6 @@
 import commonjs from 'rollup-plugin-commonjs'; // Convert CommonJS modules to ES6
 import vue from 'rollup-plugin-vue'; // Handle .vue SFC files
 import buble from 'rollup-plugin-buble'; // Transpile/polyfill with reasonable browser support
-import resolve from 'rollup-plugin-node-resolve';
 export default {
   input: 'src/wrapper.js', // Path relative to package.json
   output: {
@@ -9,7 +8,6 @@ export default {
     exports: 'named',
   },
   plugins: [
-    resolve(),
     commonjs(),
     vue({
       css: true, // Dynamically inject css as a <style> tag
